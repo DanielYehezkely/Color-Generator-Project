6585974ecab0f38842746553b0b9002248e8ec4d
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-09-07 15:33:57 +0300

    setting the color input

--- src/Form.jsx
+++ src/Form.jsx
@@ -7,6 +7,11 @@ const Form = () => {
         <section className='container'>
             <h4>Color Generator</h4>
             <form className='color-form' onSubmit={handleSubmit}>
+            <input 
+            type="color" 
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}
+            />
                 <input
                     type="text"
                     value={color}
